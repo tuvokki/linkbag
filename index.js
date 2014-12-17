42f1cf9b9fa54c7e9743a96c5c0c71d39b65698c
Wouter <tuvokki@gmail.com>
2014-12-17 11:55:44 +0100

    The links backend

--- index.js
+++ index.js
@@ -10,16 +10,23 @@ app.use(bodyParser.urlencoded({
 }));
 
 app.use(express.query());
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
 
-mongoose.connect("mongodb://localhost/resources");
+mongoose.connect("mongodb://localhost/linkbagdb");
 
-var Resource = app.resource = restful.model('resource', mongoose.Schema({
-    title: 'string',
-    year: 'number',
+var BaggedLink = app.baggedlink = restful.model('baggedlink', mongoose.Schema({
+    urlletje: String,
+    tags: [String],
+    updated: { type: Date, default: Date.now },
+    active: Boolean
   }))
   .methods(['get', 'post', 'put', 'delete']);
 
-Resource.register(app, '/resources');
+BaggedLink.register(app, '/linkbag');
 
 console.log('Starting server on port 3019 ... ');
 app.listen(3019);
